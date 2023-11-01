154324ea371d418e05dd46b92911539fa86d2f44
dlkokhta <dl.kokhtashvili@gmail.com>
2023-11-01 16:10:23 +0400

    conditionals

---
+++ 1-Level-Conditionals.js
@@ -0,0 +1,85 @@
+//1
+// const checkGrade = (score) => {
+//   if (score >= 80 && score <= 100) {
+//     return "A";
+//   } else if (score >= 70 && score <= 89) {
+//     return "B";
+//   } else if (score >= 60 && score <= 69) {
+//     return "C";
+//   } else if (score >= 50 && score <= 59) {
+//     return "D";
+//   } else {
+//     return "F";
+//   }
+// };
+// student1 = 95;
+// console.log(checkGrade(student1));
+
+//2
+// const checkSeason = (month) => {
+//   if (month === "September" || month === "October" || month === "November") {
+//     return "the season is Autumn";
+//   } else if (
+//     month === "December" ||
+//     month === "January" ||
+//     month === "February"
+//   ) {
+//     return "the season is Winter";
+//   } else if (month === "March" || month === "April" || month === "May") {
+//     return "the season is Spring";
+//   } else if (month === "June" || month === "July" || month === "August") {
+//     return "the season is Summer";
+//   } else {
+//     return "incorrect season word";
+//   }
+// };
+
+// console.log(checkSeason("May"));
+
+//3
+// const checkDay = (day) => {
+//   if (day.toLowerCase() === "saturday") {
+//     return "Saturday is a weekend.";
+//   } else if (day.toLowerCase() === "sunday") {
+//     return "Sunday is a weekend.";
+//   } else if (day.toLowerCase() === "monday") {
+//     return "Monday is a working day.";
+//   } else if (day.toLowerCase() === "tuesday") {
+//     return "Tuesday is a working day.";
+//   } else if (day.toLowerCase() === "wednesday") {
+//     return "Wednesday is a working day.";
+//   } else if (day.toLowerCase() === "thuesday") {
+//     return "Thursday is a working day.";
+//   } else if (day.toLowerCase() === "friday") {
+//     return "Friday is a working day.";
+//   }
+// };
+// console.log(checkDay("moNdaY"));
+
+//4
+// const numberOfDaysInMonth = (month) => {
+//   if (
+//     month === "january" ||
+//     month === "march" ||
+//     month === "may" ||
+//     month === "july" ||
+//     month === "august" ||
+//     month === "october" ||
+//     month === "december"
+//   ) {
+//     return "31";
+//   } else if (
+//     month === "april" ||
+//     month === "june" ||
+//     month === "september" ||
+//     month === "november"
+//   ) {
+//     return "30";
+//   } else if (month === "february") {
+//     return "28";
+//   } else {
+//     return "invalid month";
+//   }
+// };
+
+// console.log(numberOfDaysInMonth("may"));
